fd7845f4bdbf8bd76e96bb972325811a382ffe15
DESKTOP-CLMUJNV\elfle <elflee77@gmail.com>
2022-10-04 07:57:13 +0900

    Sample

--- 3DProgramming/GameObjects.hpp
+++ 3DProgramming/GameObjects.hpp
@@ -47,3 +47,62 @@ public:
         glEnd();
     }
 };
+
+
+
+class MagicCircle
+{
+public:
+    MagicCircle() {}
+    ~MagicCircle() {}
+
+    void Update()
+
+    void drawFigure()
+    {}
+
+
+    void drawEffect()
+    {}
+
+    void drawCircle()
+    {
+        glLineWidth(7.0f);
+        glBegin(GL_LINE_STRIP);
+
+        /*glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+        glVertex3f(0.0f, 0.5f, 0.0f);
+        glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+        glVertex3f(0.5f, -0.5f, 0.0f);
+        glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+        glVertex3f(-0.5f, -0.5f, 0.0f);
+        glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+        glVertex3f(0.0f, 0.5f, 0.0f);
+        glEnd();
+
+        glBegin(GL_LINE_STRIP);
+        glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+        glVertex3f(0.0f, -0.75f, 0.0f);
+        glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+        glVertex3f(0.5f, 0.25f, 0.0f);
+        glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+        glVertex3f(-0.5f, 0.25f, 0.0f);
+        glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+        glVertex3f(0.0f, -0.75f, 0.0f);
+        glEnd();
+
+        glBegin(GL_LINE_STRIP);*/
+
+        float x, y;
+        for (float theta = 0; theta < 360; theta++)
+        {
+            x = - glm::sin(glm::radians(theta)) * 0.5;
+            y = glm::cos(glm::radians(theta)) * 0.5;
+
+            glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+            glVertex3f(x, y, 0.0f);
+
+        }
+        glEnd();
+    }
+};
